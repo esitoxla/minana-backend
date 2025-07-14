9c1cda437c93cd05e6b67ea8687b37b59854f863
Irene Toxla <toxlairene@gmail.com>
2025-07-14 12:22:27 +0000

    updated cors

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", // for local dev
-      "https://minana-services-ltd.netlify.app", //  real Netlify domain
+      "https://minana-services-ltd.netlify.app", 
     ],
     credentials: true,
   })
